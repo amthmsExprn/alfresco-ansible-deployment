1d3a10e92bb239f24d0cc0a471793a3a6e0a03f4
alf-wchong <weemeng.chong@alfresco.com>
2021-08-26 19:38:06 -0400

    use internal IP

--- roles/trouter/templates/ats-atr.sh
+++ roles/trouter/templates/ats-atr.sh
@@ -7,8 +7,8 @@ fi
 . /etc/opt/alfresco/setenv.sh
 export JAVA_OPTS="${JAVA_OPTS} -DCORE_AIO_URL=http://${ATS_TENGINE_AIO_HOST}:8090"
 export JAVA_OPTS="${JAVA_OPTS} -DCORE_AIO_QUEUE=org.alfresco.transform.engine.aio.acs"
-export JAVA_OPTS="${JAVA_OPTS} -DACTIVEMQ_URL=failover:(tcp://{{ activemq_host }}:61616)?timeout=3000"
-export JAVA_OPTS="${JAVA_OPTS} -DFILE_STORE_URL=http://{{ sfs_host }}:8099/alfresco/api/-default-/private/sfs/versions/1/file"
+export JAVA_OPTS="${JAVA_OPTS} -DACTIVEMQ_URL=failover:(tcp://{{ activemq_host_internal }}:61616)?timeout=3000"
+export JAVA_OPTS="${JAVA_OPTS} -DFILE_STORE_URL=http://{{ sfs_host_internal }}:8099/alfresco/api/-default-/private/sfs/versions/1/file"
 {% for key, value in trouter_environment.items() %}
 export {{key}}="{{value}}"
 {% endfor %}
